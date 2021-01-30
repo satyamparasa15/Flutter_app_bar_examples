b6f0b13f023dcba61b2d51c76936cd4b613c0aa3
Satyam parasa <sathyam.parasa@gmail.com>
2021-01-30 13:45:25 +0530

    added Messenger app bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: new ThemeData(),
-      home: YoutubeAppBar(),
+      home: MessengerAppBar(),
       debugShowCheckedModeBanner: false,
     );
   }
