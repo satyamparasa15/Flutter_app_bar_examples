7f19cd0a2735c81e6906e51ef17a62c2ff773445
Satyam parasa <sathyam.parasa@gmail.com>
2021-01-30 13:30:11 +0530

    added youtube app bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: new ThemeData(),
-      home: WhatsAppAppBar(),
+      home: YoutubeAppBar(),
       debugShowCheckedModeBanner: false,
     );
   }
